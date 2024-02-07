4ca6aef7cb6f25fcaf2fd0731a50c94dc8fda227
Serhii Zakutko <sergeyzakutko2000@gmail.com>
2024-02-07 12:31:53 +0100

    initial commit on develop branch

--- sz-form-elements-library-demo-app/src/app/app.component.spec.ts
+++ sz-form-elements-library-demo-app/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'sz-form-elements-library-demo-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sz-form-elements-library-demo-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sz-form-elements-library-demo-app app is running!');
-  });
 });
--- sz-form-elements-library-demo-app/src/app/app.component.ts
+++ sz-form-elements-library-demo-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sz-form-elements-library-demo-app';
+
 }
